9f9f8694cc7be0b5adaf62bb9e9097444dba6ec1
Abdulrahman Alaa Eldin  <abdulrahmanalaa@hotmail.com>
2020-04-19 06:57:15 +0400

    inial commit

---
+++ node_visits/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const redis = require('redis');
+const app = express();
+
+const client = redis.createClient({
+  host: 'redis-server',
+  port: 6379,
+});
+client.set('visits', 0);
+app.get('/', (req, res) => {
+  client.get('visits', (error, visits) => {
+    res.send(visits);
+    client.set('visits', +visits + 1);
+  });
+});
+
+app.listen(8080, () => {
+  console.log('running on port', 8080);
+});
